7406f7f6b6e787c3976b6f8ff29dcf6400b2a131
abanoubm1234567 <amasoud1234567@gmail.com>
2024-10-05 20:10:36 -0400

    initial commit

---
+++ src/Profile.js
@@ -0,0 +1,12 @@
+import React, { Component } from '../public/node_modules/@types/react';
+class Profile extends Component {
+
+  render() {
+    return (
+      <div>
+       Hello from Profile Component
+      </div>
+    );
+  }
+}
+export default Profile;
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App prop1="hello"></App>
   </React.StrictMode>
 );
 
