c3003008c58ac6c91412da9d582b4e9e15a59d0a
abanoubm1234567 <amasoud1234567@gmail.com>
2024-10-07 16:51:59 -0400

    whole bunch of stuff

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import "./App.css";
 import Header from './Header';
 import PersonalProfile from './PersonalProfile';
+import WorkExperience from './WorkExperience';
 class App extends Component {
   constructor(props) {
     super(props)
@@ -17,17 +18,25 @@ class App extends Component {
       },
       titles: {
         pp: "Personal Profile",
+        we: "Work Experience",
       },
       description: {
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laborisnisi ut aliquip ex ea commodo consequat.",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laborisnisi ut aliquip ex ea commodo consequat.",
       },
+      experiences: {
+        exp1: "Job Title at Company (August 2022 – December 2023)",
+        exp2: "Job Title 2 at Company 2 (August 2020 – December 2021)",
+      }
     }
   }
   render() {
     return (
       <div>
         <Header personInfo={this.state.personInfo} contactInfo={this.state.contactInfo}></Header>
+        <div className="yellowDiv"></div>
         <PersonalProfile titles={this.state.titles} description={this.state.description}></PersonalProfile>
+        <div className='greyDiv'></div>
+        <WorkExperience titles={this.state.titles} description={this.state.description} experiences={this.state.experiences}></WorkExperience>
       </div>
     );
   }
--- src/WorkExperience.js
+++ src/WorkExperience.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 
-class PersonalProfile extends Component {
+class WorkExperience extends Component {
   render() {
     return (
       <div className="hd2">
         <div className="hd2_c1">
           <div>
-            <p className="title">{this.props.titles.pp}</p>
+            <p className="title">{this.props.titles.we}</p>
           </div>
         </div>
         <div className="hd2_c2">
           <div>
+            <p className="title">{this.props.experiences.exp1}</p>
+            <p>{this.props.description.desc}</p>
+            <p className="title">{this.props.experiences.exp2}</p>
             <p>{this.props.description.desc}</p>
           </div>
         </div>
@@ -19,4 +22,4 @@ class PersonalProfile extends Component {
   }
 }
 
-export default PersonalProfile;
+export default WorkExperience;
